5ba051872e1e825f17c8bd3d1359a25059277294
AceCoder3 <107264922+AceCoder3@users.noreply.github.com>
2022-06-10 11:09:14 -0400

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -7,3 +7,10 @@ for i in range (1,44):
   if count==0:
     b.append(i)
 c=tuple(b)
+z=list(b)
+k=0
+for i in range(0,5):
+  for j in range(0,i+1):
+    print(z[k],end=" ")
+    k=k+1
+  print("\n")
